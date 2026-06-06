5a8ee4314b87068242d369015e112f883dacd267
Katherine <sdany237@estudiantec.cr>
2026-06-05 18:53:36 -0600

    Actualizar setup y correccion de target solucion

---
+++ Tutorial_Guiado/build/CMakeFiles/taller.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/event_groups.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/event_groups.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/event_groups.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/list.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/list.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/list.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/portable/MemMang/heap_4.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/MemMang/heap_4.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/MemMang/heap_4.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/port.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/port.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/port.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/utils/wait_for_event.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/utils/wait_for_event.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/utils/wait_for_event.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/queue.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/queue.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/queue.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/stream_buffer.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/stream_buffer.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/stream_buffer.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/tasks.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/tasks.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/tasks.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/FreeRTOS-Kernel/timers.c" "CMakeFiles/taller.dir/FreeRTOS-Kernel/timers.c.o" "gcc" "CMakeFiles/taller.dir/FreeRTOS-Kernel/timers.c.o.d"
+  "/home/hella/Taller_Embebidos/Minitaller/Minitaller_FreeRTOS/Tutorial_Guiado/src/taller.c" "CMakeFiles/taller.dir/src/taller.c.o" "gcc" "CMakeFiles/taller.dir/src/taller.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Tutorial_Guiado/build/CMakeFiles/taller.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/event_groups.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/event_groups.c.o.d"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/list.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/list.c.o.d"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/MemMang/heap_4.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/MemMang/heap_4.c.o.d"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/port.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/port.c.o.d"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/utils/wait_for_event.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/portable/ThirdParty/GCC/Posix/utils/wait_for_event.c.o.d"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/queue.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/queue.c.o.d"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/stream_buffer.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/stream_buffer.c.o.d"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/tasks.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/tasks.c.o.d"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/timers.c.o"
+  "CMakeFiles/taller.dir/FreeRTOS-Kernel/timers.c.o.d"
+  "CMakeFiles/taller.dir/src/taller.c.o"
+  "CMakeFiles/taller.dir/src/taller.c.o.d"
+  "taller"
+  "taller.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/taller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
